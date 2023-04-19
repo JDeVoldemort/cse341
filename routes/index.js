d0982fd26498464e318ce73d6d0c33fb9509ce39
JDVoldemort <dragonlore8@gmail.com>
2023-04-19 17:32:30 -0600

    try these fixes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 const names = require('../controllers');
 
-routes.get('/', names.displayName);
+routes.get('/', names.displayName); //localhost:3000
 
 module.exports = routes;
