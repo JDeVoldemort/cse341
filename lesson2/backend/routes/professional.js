ebae154f9872d8f4ad1dec5f1f08471bff7c4e29
JDVoldemort <dragonlore8@gmail.com>
2023-04-29 16:32:11 -0600

    Test for working

--- lesson2/backend/routes/professional.js
+++ lesson2/backend/routes/professional.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const professionalController = require('../controllers/professional');
 
-const router = express.Router();
+const routes = express.Router();
 
 // GET /feed/posts
-router.get('/', professionalController.getData);
+routes.get('/', professionalController.getData);
 // localhost:8080/professional/
-module.exports = router;
+module.exports = routes;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const names = require('../controllers');
 
-routes.get('/', names.displayName); //localhost:3000
+routes.get('/', names.displayName); //localhost:8080
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
