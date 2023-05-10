3eded2aba7e86dcef6903eeabed9cb6ec935b2f3
JDVoldemort <dragonlore8@gmail.com>
2023-05-09 18:27:38 -0600

    test render pass

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const names = require('../controllers');
 
+routes.use('/', require('./swagger'));
+
 routes.get('/', names.displayName); //localhost:8080
 routes.use('/contacts', require('./contacts'))
 
