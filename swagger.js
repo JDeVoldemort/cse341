124547bf360d4cfb50d78b8257e3e3fde5a59502
JDVoldemort <dragonlore8@gmail.com>
2023-05-09 23:36:22 -0600

    try this

--- swagger.js
+++ swagger.js
@@ -5,17 +5,17 @@ const doc = {
     title: 'My API',
     description: 'Contacts API',
   },
-  host: 'cse-341-test-dhh4.onrender.com/',
-  schemes: ['http'],
+  host: 'cse-341-test-dhh4.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
 // generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
+// swaggerAutogen(outputFile, endpointsFiles, doc);
 
 // Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+  await import('./index.js');
+});
